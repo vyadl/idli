f1faf83339073f79aa5ff4e459acd195644f8df4
octo <octo@octos-MacBook-Pro.local>
2022-04-05 15:48:36 +0300

    add deployment script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source /home/vyadl/nodevenv/idli/14/bin/activate && cd /home/vyadl/idli;
+git pull "https://vyadl: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@github.com/vyadl/idli.git" dev;
+npm install;
+npm run build;
