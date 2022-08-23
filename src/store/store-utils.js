9724c81f4d62733b9c8605e2430c22874f244220
hellgaynius <tnuocc@gmail.com>
2022-08-23 12:18:20 +0300

    cr fixes: naming in utils for vuex root calls

--- src/store/store-utils.js
+++ src/store/store-utils.js
@@ -1,9 +1,9 @@
 import store from '@/store/index'; // eslint-disable-line import/no-cycle
 
-export function dispatchFromRoot(methodName, payload = null) {
-  store.dispatch(methodName, payload, { root: true });
+export function dispatchFromRoot(actionName, payload = null) {
+  store.dispatch(actionName, payload, { root: true });
 }
 
-export function commitFromRoot(methodName, payload = null) {
-  store.commit(methodName, payload, { root: true });
+export function commitFromRoot(mutationName, payload = null) {
+  store.commit(mutationName, payload, { root: true });
 }
